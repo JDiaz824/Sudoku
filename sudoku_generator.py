bb520de5cd7b68658f7055f05629a2f3c46d4e1b
patrick-ouano <patrick.ouano@ufl.edu>
2024-12-02 15:01:44 -0500

    -deleted self.row_length

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -23,7 +23,6 @@ class SudokuGenerator:
 	None
     '''
     def __init__(self, row_length, removed_cells):
-        self.row_length = row_length
         self.removed_cells = removed_cells
         self.board = []
         self.box_length = math.sqrt(row_length)
