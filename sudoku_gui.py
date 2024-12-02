b85cdc7261da99928bece2f5e25eb75d37d3b720
JDiaz824 <johan.dia824@gmail.com>
2024-12-02 18:18:51 -0500

    Game WOn

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -54,6 +54,50 @@ def draw_game_start(screen):
                     sys.exit()
         pygame.display.update()
 
+def draw_game_won(screen):
+    # Initialized title font
+    win_title_font = pygame.font.Font(None, 100)
+    button_font = pygame.font.Font(None, 70)
+
+    # Color background
+    screen.fill(BG_COLOR)
+
+    # Initialize and draw title
+    title_surface = win_title_font.render("Game Won!", 0, LINE_COLOR)
+    title_rectangle = title_surface.get_rect(
+        center = (WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title_surface, title_rectangle)
+
+    # Initialize buttons
+    # Initialize text first
+    exit_text = button_font.render("Exit", 0, (255, 255, 255))
+    quit_text = button_font.render("Quit", 0, (255, 255, 255))
+
+    # Initialize button background color and text
+    exit_surface = pygame.Surface((exit_text.get_size()[0] + 20, exit_text.get_size()[1] + 20))
+    exit_surface.fill(LINE_COLOR)
+    exit_surface.blit(exit_text, (10, 10))
+
+    # Initialize button rectangle
+    exit_rectangle = exit_surface.get_rect(
+        center = (WIDTH // 2, HEIGHT // 2 + 150))
+
+    # Draw buttons
+    screen.blit(exit_surface, exit_rectangle)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Checks if mouse is on exit button
+                if exit_rectangle.collidepoint(event.pos):
+                    # If the mouse is on the quit button, exits system
+                    sys.exit()
+
+        pygame.display.update()
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
