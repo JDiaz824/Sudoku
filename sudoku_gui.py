a6a7b47199227b3ab9a64174d4328d060039d5ba
patrick-ouano <patrick.ouano@ufl.edu>
2024-12-02 16:23:53 -0500

    -added sudoku_gui

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -1,1 +1,24 @@
 # sudoku gui
+
+import pygame, sys
+
+def main():
+    try:
+        pygame.init()
+        screen = pygame.display.set_mode((700, 700))
+        pygame.display.set_caption("Sudoku")
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    print(event.pos)
+
+                screen.fill("White")
+                pygame.display.flip()
+    finally:
+        pygame.quit()
+
+if __name__ == "__main__":
+    main()
