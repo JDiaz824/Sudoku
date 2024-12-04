a9a0eff0154adc604ad2a7600e05813343c93de8
JDiaz824 <johan.dia824@gmail.com>
2024-12-03 20:17:18 -0500

    Added Sketch programming

--- sudoku_cell.py
+++ sudoku_cell.py
@@ -3,7 +3,7 @@ import pygame
 class Cell:
     def __init__(self, value, row, col, screen):
         self.value = value
-        self.sketeched_value = value
+        self.sketeched_value = None
         self.row = row
         self.col = col
         self.screen = screen
@@ -25,8 +25,15 @@ class Cell:
         pygame.draw.rect(self.screen, cell_color, square, cell_width)
 
         if self.value != 0:
-            number_font = pygame.font.Font(None, 40)
+            number_font = pygame.font.Font(None, 60)
             number_surface = number_font.render(str(self.value), 0, "Black")
             number_rectangle = number_surface.get_rect(
                 center=(self.col*70 + 70/2, self.row*70 + 70/2))
+            self.screen.blit(number_surface, number_rectangle)
+
+        if (self.sketeched_value is not None):
+            number_font = pygame.font.Font(None, 50)
+            number_surface = number_font.render(str(self.sketeched_value), 0, (128, 128, 128))
+            number_rectangle = number_surface.get_rect(
+                center=(self.col * 70 + 15, self.row * 70 + 20))
             self.screen.blit(number_surface, number_rectangle)
